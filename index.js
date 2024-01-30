4c3fd47e14efd0c0e936fc48299e40e5b7a1b09c
Sam <zaaamuel_96@hotmail.com>
2024-01-30 21:58:53 +0100

    added search button features

--- index.js
+++ index.js
@@ -68,3 +68,22 @@ function handleFetchError(error) {
     alert('Failed to fetch data. Please try again later.');
 }
 
+
+document.addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        searchImages();
+    }
+});
+
+document.getElementById('searchInput').addEventListener('click', function() {
+    // Ensure the search input regains focus when clicked
+    this.focus();
+});
+
+// Additional code to handle the 'click' event on the document if needed
+document.addEventListener('click', function(event) {
+    // Check if the click target is not the search input
+    if (event.target.id !== 'searchInput') {
+        searchImages();
+    }
+});
